4464b6512ed3e0b8c986d45fdf9523fd02586545
CJKim29 <kchanjong29@gmail.com>
2024-08-26 19:18:11 +0900

    hall

---
+++ src/main/java/com/githrd/project3/dao/BookMapper.java
@@ -0,0 +1,13 @@
+package com.githrd.project3.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.githrd.project3.vo.BookVo;
+
+@Mapper
+public interface BookMapper {
+
+    List<BookVo> selectList();
+}
