2da146ba539cb11fe7a242b4d03321afa31515ec
CJKim29 <kchanjong29@gmail.com>
2024-10-02 09:46:00 +0900

    feat. mypage 최신 mem_point 반영

--- src/main/java/com/githrd/project3/dao/MemberMapper.java
+++ src/main/java/com/githrd/project3/dao/MemberMapper.java
@@ -9,23 +9,25 @@ import com.githrd.project3.vo.MemberVo;
 @Mapper
 public interface MemberMapper {
 
-    List<MemberVo> selectList();
+ List<MemberVo> selectList();
 
-    MemberVo selectOneFromIdx(int mem_idx);
+ MemberVo selectOneFromIdx(int mem_idx);
 
-    MemberVo selectOneFromId(String mem_id);
+ MemberVo selectOneFromId(String mem_id);
 
-    MemberVo selectOne_nickname(String mem_nickname);
+ MemberVo selectOne_nickname(String mem_nickname);
 
-    int insert(MemberVo vo);
+ int insert(MemberVo vo);
 
-    int update(MemberVo vo);
+ int update(MemberVo vo);
 
-    int delete(int mem_idx);
+ int delete(int mem_idx);
 
-    void updateFilename(MemberVo vo);
+ void updateFilename(MemberVo vo);
 
-    MemberVo selectOne_phone(String mem_phone);
+ MemberVo selectOne_phone(String mem_phone);
 
-    MemberVo selectOne_email(String mem_email);
+ MemberVo selectOne_email(String mem_email);
+
+ int getMem_point(int mem_idx);
 }
